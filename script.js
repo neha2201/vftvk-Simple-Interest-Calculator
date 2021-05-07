3c372fa84a01773146cc6d1541ba3ba3d64bae01
Neha Kumari <nehaagarwal750@gmail.com>
2021-05-07 12:29:56 +0000

    All changes hs done successfully

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ function compute()
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
-    var interest =(principal * years * rate) / 100;
     var year = new Date().getFullYear()+parseInt(years);
+    var interest =(principal * years * rate) / 100;
     if (principal < 1){
         alert("Enter a positive number");
         
